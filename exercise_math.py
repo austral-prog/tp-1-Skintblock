88a585421d6edc8d0bbfa331dc59f24512adb02e
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-03-25 19:25:13 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,20 @@
 def math():
     a = 57
     b = 7
+    #operaciones
+    sum = a + b
+    dif = a - b
+    prod = a * b
+    prom = (a + b) / 2
+    cociente_entero = a // b
+    resto = a % b
+    coc_real = a / b
+
+    #imprimir resultados
+    print(sum)
+    print(dif)
+    print(prod)
+    print(prom)
+    print(cociente_entero)
+    print(resto)
+    print(coc_real)
